fc1c9c3a9a931618c6641690611845cd6611038d
Mark Silva <mark.allen.silva@gmail.com>
2022-08-27 16:56:55 -0700

    add logic for user interaction and styles

--- index.js
+++ index.js
@@ -5,44 +5,48 @@ let sum;
 let randomCard;
 let message = "";
 
+let messageEl = document.getElementById("message-el");
+
 let hasBlackjack = false;
 let isAlive = true;
 
+function startGame() {
 // create function that randomly generate a value between 2 -11
-function random() {
-  let min = Math.ceil(2);
-  let max = Math.floor(12);
-  randomCard = Math.floor(Math.random() * (max - min) + min);  
-}
+  function random() {
+    let min = Math.ceil(2);
+    let max = Math.floor(12);
+    randomCard = Math.floor(Math.random() * (max - min) + min);  
+  }
 
-random();
-firstCard = randomCard;
-console.log(firstCard);
+  random();
+  firstCard = randomCard;
+  console.log(firstCard);
+  //document.getElementById("card1").textContent = firstCard;
 
-random();
-secondCard = randomCard;
-console.log(secondCard);
+  random();
+  secondCard = randomCard;
+  console.log(secondCard);
+  //document.getElementById("card2").textContent = secondCard;
 
-sum = firstCard + secondCard + 5;
-console.log("Sum: " + sum);
+  sum = firstCard + secondCard + 5;
+  console.log("Sum: " + sum);
 
-if (sum <= 20)  {
-  message = "Would you like another card?";
-  // if user says yes, 
-    //random();
-    //sum+= randomCard;
-} else if (sum === 21) {
-    message = "BLACKJACK! You win!";
-    hasBlackjack = true;
+  if (sum <= 20)  {
+    message = "Would you like another card?";
+    // if user says yes, 
+      //random();
+      //sum+= randomCard;
+  } else if (sum === 21) {
+      message = "BLACKJACK! You win!";
+      hasBlackjack = true;
+      
+  } else {
+      message = "BUSTED! You lose!";
+      isAlive = false;
     
-} else {
-    message = "BUSTED! You lose!";
-    isAlive = false;
-  
+  }
+
+  messageEl.textContent = message;
+  console.log("Has blackjack: " + hasBlackjack);
+  console.log("Is alive: " + isAlive);
 }
-
-console.log(message);
-console.log("Has blackjack: " + hasBlackjack);
-console.log("Is alive: " + isAlive);
-
-
