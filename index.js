76735c54eff8196d56dd861c3341f73361f47e6a
Mark Silva <mark.allen.silva@gmail.com>
2022-08-28 14:14:43 -0700

    addCard() funcrtion now working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-
+let hasBlackjack = false;
+let isAlive = true;
 let firstCard;
 let secondCard;
-let addCard;
 let sum;
 let randomCard;
 let message = "";
@@ -10,9 +10,6 @@ let messageEl = document.getElementById("message-el");
 let cardsEl = document.getElementById("cards-el");
 let cardSumEl = document.getElementById("cardSum-el");
 
-let hasBlackjack = false;
-let isAlive = true;
-
 // create function that randomly generate a value between 2 -11
 function random() {
   let min = Math.ceil(2);
@@ -20,48 +17,46 @@ function random() {
   randomCard = Math.floor(Math.random() * (max - min) + min);  
 }
 
-function newCard() {
-  random();
-  addCard = randomCard;
-  console.log(addCard);
-}
-
-function startGame() {
-
   random();
   firstCard = randomCard;
   console.log(firstCard);
-  //document.getElementById("card1").textContent = firstCard;
 
   random();
   secondCard = randomCard;
   console.log(secondCard);
-  //document.getElementById("card2").textContent = secondCard;
 
   sum = firstCard + secondCard;
   console.log("Sum: " + sum);
-  
 
+function startGame() {
+  
+  cardsEl.textContent = firstCard + ", " + secondCard;
+  cardSumEl.textContent = sum;
+  
   if (sum <= 20)  {
     message = "Would you like another card?";
 
   } else if (sum === 21) {
       message = "BLACKJACK! You win!";
       hasBlackjack = true;
-      
+        
   } else {
       message = "BUSTED! You lose!";
       isAlive = false;
-    
+      
   }
   
-  
-  newCard();
-  cardsEl.textContent = firstCard + ", " + secondCard;
-  cardSumEl.textContent = sum;
   messageEl.textContent = message;
-
 
   console.log("Has blackjack: " + hasBlackjack);
   console.log("Is alive: " + isAlive);
+}
+
+function newCard() {
+  random();
+  let drawCard = randomCard;
+  sum += drawCard;
+  console.log("New Card: " + drawCard);
+  console.log("New Sum: " + sum);
+  startGame();
 }
